be0d921a297ba52a3d84c0223c65585e5117cf2d
peter <peterwanjohi143@gmail.com>
2021-02-15 15:55:32 +0300

    update senderId controller

--- routes/senderid.route.js
+++ routes/senderid.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // import controller
 const { requireSignin, superAdminMiddleware } = require('../controllers/auth.controller');
-const { readAllController, readSingleController, saveController, approveController, deleteController } = require('../controllers/senderidRequest.controller');
+const { readAllController, readSingleController, saveController, approveController, deleteController } = require('../controllers/senderIdRequest.controller');
 
 router.get('/senderid/:name', requireSignin,superAdminMiddleware, readSingleController);
 router.get('/senderids', requireSignin,superAdminMiddleware, readAllController);
